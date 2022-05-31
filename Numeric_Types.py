ed31307a70523dca262e0de19842dbe7ba14188d
Jesper Åkerberg <criperq@gmail.com>
2022-05-31 21:38:56 +0200

    update

--- Numeric_Types.py
+++ Numeric_Types.py
@@ -1,1 +1,17 @@
-print("Hello")
+# Integers kan kodas i andra format: 
+print(0x01) # Hexadecimal 0-9, A-F
+print(0o01) # Octal 1-7
+print(0b00000101) # Binary 0-1 där längst åt höger är 1, och vänster 128.
+
+# Få fram andra kodningar
+x = 10
+hex(x)
+oct(x)
+bin(x)
+
+# Skapa komplexa tal
+y = complex(3,5)
+print(y)
+
+# Expression Operators
+
