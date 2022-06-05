42a2dfec74d849124b02d47b431c48cbf5c5bd18
jesper1991 <criperq@gmail.com>
2022-06-05 21:32:13 +0200

    up

--- Methods_And_Functions.py
+++ Methods_And_Functions.py
@@ -1,4 +1,7 @@
 
+import re
+
+
 mylist = [1,2,3]
 mylist.append(4)
 mylist.pop()
@@ -7,3 +10,23 @@ mylist
 # How to get a list of methods for an object - t.ex. mylist.punkt för att se alla
 mylist.insert
 help(mylist.insert)
+
+# def keyword
+
+# def name_of_function():
+
+def test():
+    '''
+    Explain function.
+    '''
+    print("Hello")
+
+    test() # call function
+
+# Can accept argument
+
+def add_function(num1,num2):
+    return num1+num2
+
+result = add_function(1,2)
+print(result)
